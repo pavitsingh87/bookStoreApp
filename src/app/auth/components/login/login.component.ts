3b1c9d4919d0a4bda0930845a7e6879e5d3f6c79
pavitsingh87 <pavitsinghmakkar@gmail.com>
2023-10-29 12:24:43 -0500

    Get query string values from url

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  name:string="";
+  constructor(private route: ActivatedRoute){
+
+  }
+  ngOnInit(){
+    this.route.queryParams.subscribe((param)=>{
+      this.name=param["name"];
+    })
+  }
 }
