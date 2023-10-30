d71edb10331f5bb5ecb27651f1d8426b55a79cf6
pavitsingh87 <pavitsinghmakkar@gmail.com>
2023-10-29 23:01:55 -0500

    angular table and angular form example '

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,40 @@ import { UserModule } from './user/user.module';
 import { PublicModule } from './public/public.module';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddressComponent } from './address/address.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { TableComponent } from './table/table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    HowItWorksComponent
+    HowItWorksComponent,
+    AddressComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
     SharedModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
